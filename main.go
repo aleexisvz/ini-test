4b1844552c9cc9ff6b51179c301a2b3a7c2bea99
aleexisvz <alexiis1998@hotmail.com>
2023-11-16 11:46:26 -0300

    get closest key

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -15,17 +16,43 @@ type TarjetaPVC struct {
 	TypeImpression string
 }
 
-func calculatePrice(t *TarjetaPVC) {
+func roundToNearest(t *TarjetaPVC, cfg *ini.File) {
+	// save keys and value on keyMap
+	keyMap := make(map[string]string)
+
+	if t.TypeImpression == "SIMPLE FAZ" {
+		for _, key := range cfg.Section("SIMPLEFAZ").Keys() {
+			keyMap[key.Name()] = key.Value()
+		}
+	} else {
+		for _, key := range cfg.Section("DOBLEFAZ").Keys() {
+			keyMap[key.Name()] = key.Value()
+		}
+	}
+
+	// get closest key
+	var closestKey int
+	amount, _ := strconv.Atoi(t.Amount)
+	minDiff := math.MaxInt
+	for keyStr := range keyMap {
+		keyInt, _ := strconv.Atoi(keyStr)
+		diff := int(math.Abs(float64(keyInt - amount)))
+		if diff < minDiff {
+			minDiff = diff
+			closestKey = keyInt
+		}
+	}
+
+	closestKeyStr := strconv.Itoa(closestKey)
+	result, _ := strconv.Atoi(keyMap[closestKeyStr])
+
+	t.Price = strconv.Itoa(result)
+}
+
+func calculatePrice(t *TarjetaPVC, cfg *ini.File) {
 	// vars
 	var price int
 	var amount int
-
-	// load config file
-	cfg, err := ini.Load("config.ini")
-	if err != nil {
-		fmt.Printf("Fail to read file: %v", err)
-		os.Exit(1)
-	}
 
 	// set amount
 	fmt.Print("Enter amount: ")
@@ -40,19 +67,24 @@ func calculatePrice(t *TarjetaPVC) {
 	if strings.ToLower(dobleFaz) == "y" {
 		t.TypeImpression = "DOBLE FAZ"
 
-		// set doblefaz price
-		t.Price = cfg.Section("DOBLEFAZ").Key(t.Amount + "UNI").String()
+		// round to nearest
+		roundToNearest(t, cfg)
+
 		price, _ = strconv.Atoi(t.Price)
 		amount, _ = strconv.Atoi(t.Amount)
+
 		price = price * amount
 		t.Price = strconv.Itoa(price)
 	} else {
 		t.TypeImpression = "SIMPLE FAZ"
 
-		// set simplefaz price
-		t.Price = cfg.Section("SIMPLEFAZ").Key(t.Amount + "UNI").String()
+		// round to nearest
+		roundToNearest(t, cfg)
+
 		price, _ = strconv.Atoi(t.Price)
 		amount, _ = strconv.Atoi(t.Amount)
+
+		// calculate & set price
 		price = price * amount
 		t.Price = strconv.Itoa(price)
 	}
@@ -60,7 +92,15 @@ func calculatePrice(t *TarjetaPVC) {
 
 func main() {
 	var t TarjetaPVC
-	calculatePrice(&t)
+
+	// load config file
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	calculatePrice(&t, cfg)
 
 	fmt.Printf("%su. Tarjetas PVC %s te salen $%s", t.Amount, t.TypeImpression, t.Price)
 }
--- testing.go
+++ testing.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strconv"
 
 	"gopkg.in/ini.v1"
 )
@@ -14,14 +16,30 @@ func testing() {
 		os.Exit(1)
 	}
 
+	// save keys and value on keyMap
 	keyMap := make(map[string]string)
 	for _, key := range cfg.Section("SIMPLEFAZ").Keys() {
 		keyMap[key.Name()] = key.Value()
 	}
 
-	for k, v := range keyMap {
-		fmt.Println(k, v)
+	var value int
+	value = 1565
+
+	var closestKey int
+	minDiff := math.MaxInt
+	for keyStr := range keyMap {
+		keyInt, _ := strconv.Atoi(keyStr)
+		diff := int(math.Abs(float64(keyInt - value)))
+		if diff < minDiff {
+			minDiff = diff
+			closestKey = keyInt
+		}
 	}
+
+	closestKeyStr := strconv.Itoa(closestKey)
+	result, _ := strconv.Atoi(keyMap[closestKeyStr])
+
+	fmt.Printf("%du. Tarjetas PVC SIMPLE FAZ te salen $%d", value, result)
 }
 
 func main() {
